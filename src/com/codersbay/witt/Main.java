fd95fdf9d3169f72926356206ad57edfd325763e
Thomas Witt <tom.89@gmx.at>
2020-02-10 11:03:54 +0100

    Add Main method

--- src/com/codersbay/witt/Main.java
+++ src/com/codersbay/witt/Main.java
@@ -2,7 +2,7 @@ package com.codersbay.witt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello my hero!");
     }
 
 }
